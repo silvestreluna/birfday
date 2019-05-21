523b5348cb5d6b318ec4d6dbbcb5480fbe6a3d00
Silvestre Luna <slvtrluna@gmail.com>
2019-05-20 21:27:13 -0500

    completed auth. User can log in and log out

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,10 +2,15 @@ import firebase from 'firebase/app';
 import auth from './components/auth/auth';
 import birfday from './components/birfday/birfday';
 import apiKeys from './helpers/apiKeys.json';
+import myNavBar from './components/myNavBar/myNavBar';
+import authData from './helpers/data/authData';
+
 import '../styles/main.scss';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
+  myNavBar.navbarEvents();
+  authData.checkLoginStatus();
   auth.authPrint();
   birfday.birfdayPrint();
 };
