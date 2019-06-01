e55fa796d8d193f215472d60f91a648af41e33fc
Silvestre Luna <slvtrluna@gmail.com>
2019-06-01 10:02:18 -0500

    able to print new prints to the dom

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -19,7 +19,7 @@ const checkLoginStatus = () => {
       authNavbar.classList.add('hide');
       logoutNavbar.classList.remove('hide');
       birfday.birfdayPrint(user.uid);
-      friends.showFriends();
+      friends.getFriends(user.uid);
     } else {
       authDiv.classList.remove('hide');
       birfdayDiv.classList.add('hide');
--- src/javascripts/helpers/data/friendsData.js
+++ src/javascripts/helpers/data/friendsData.js
@@ -3,6 +3,20 @@ import apiKeys from '../apiKeys.json';
 
 const firebaseUrl = apiKeys.firebaseKeys.databaseURL;
 
+const getFriendsByUid = uid => new Promise((resolve, reject) => {
+  axios.get(`${firebaseUrl}/friends.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((results) => {
+      const friendResults = results.data;
+      const friends = [];
+      Object.keys(friendResults).forEach((friendId) => {
+        friendResults[friendId].id = friendId;
+        friends.push(friendResults[friendId]);
+      });
+      resolve(friends);
+    })
+    .catch(err => reject(err));
+});
+
 const addNewFriend = friendObject => axios.post(`${firebaseUrl}/friends.json`, friendObject);
 
-export default { addNewFriend };
+export default { addNewFriend, getFriendsByUid };
